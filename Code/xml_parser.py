91f1123ca71f486dc378a8ab85b9b1decdfec7e2
vdsalex <voaides.alex@gmail.com>
2018-12-04 11:06:54 +0200

    modified xml_parser.py

--- Code/xml_parser.py
+++ Code/xml_parser.py
@@ -1,7 +1,6 @@
 import re
 
-path_to_articles = 'articles-training-20180831.xml'
-xml_file = open(path_to_articles, 'r')
+xml_file = open(r'E:\articles-training-20180831.xml', 'r')
 xml_out = open(r'xml_out.xml', 'w')
 parsed_xml = open(r'parsed_xml.txt', 'w')
 
@@ -10,7 +9,6 @@ data = xml_file.read(bytes_to_read)
 xml_out.write(data)
 
 article_regex = re.compile('(<article.*?>.*?</article>)', re.DOTALL)
-get_title_from_article_regex = re.compile('title=\"(.*?)\"')
 p_tag_regex = re.compile('<p>(.*?)</p>')
 a_tag_regex = re.compile('(<a.*?>.*?</a>)')
 a_tag_content_regex = re.compile('<a.*?>(.*?)</a>')
@@ -18,8 +16,6 @@ a_tag_content_regex = re.compile('<a.*?>(.*?)</a>')
 articles = article_regex.findall(data)
 
 for article in articles:
-    parsed_xml.write(get_title_from_article_regex.findall(article)[0])
-    parsed_xml.write('\n\n')
     ps = p_tag_regex.findall(articles[0])
     for i in range(0, len(ps)):
         a_tags = a_tag_regex.findall(ps[i])
